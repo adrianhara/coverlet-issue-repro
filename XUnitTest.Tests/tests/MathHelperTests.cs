f218f6c3556b9c14ea7cfa311519e542f2368479
Adrian Hara <info@adrianhara.com>
2019-09-18 20:24:14 +0200

    Initial commit.

---
+++ XUnitTest.Tests/tests/MathHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace XUnitTest.Tests
+{
+    public class MathHelperTests
+    {
+        [Fact]
+        public void WhenBothOperandsArePositive_ReturnsCorrectResult()
+        {
+            var sut = new MathHelper();
+
+            var result = sut.Add(1, 2);
+
+            Assert.Equal(3, result);
+        }
+
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,3,5)]
+        public void TestAddWithVariousInputs(int op1, int op2, int expectedResult)
+        {
+            var sut = new MathHelper();
+
+            var result = sut.Add(op1, op2);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+
+   
+}
---
+++ XUnitTest/MathHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitTest
+{
+    public class MathHelper
+    {
+        public int Add(int a1, int b)
+        {
+            if (a1 < 0)
+            {
+                return 0;
+            }
+
+            return a1 + b;
+        }
+    }
+}
